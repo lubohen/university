d745d2d32fcd27147de47ba1dc606852ae0c4514
lubohen <lbhenrique.infonegocios@gmail.com>
2020-12-03 02:36:39 -0300

    TCC_CONSERVACAO

--- Codes/Teste_db.py
+++ Codes/Teste_db.py
@@ -6,11 +6,11 @@ if conexao.is_connected():
     db_info = conexao.get_server_info()
     print("conectado ao servidor mysql", db_info)
     cursor = conexao.cursor()
-    cursor.execute("select database();")
-    linha = cursor.fetchone()
-    print("Conectado ao ", linha)
+    cursor.execute("show tables;")
 
-if conexao.is_connected():
-    cursor.close()
-    conexao.close()
-    print("Conexão Encerrada")
+
+
+# if conexao.is_connected():
+#     cursor.close()
+#     conexao.close()
+#     print("Conexão Encerrada")
